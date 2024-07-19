f57d06e4aadabd95233f4639d95c2ec93573d375
4shadoww <4shadoww0@gmail.com>
2024-07-19 23:27:10 +0300

    Add xml header

--- python-sitemap-generator.py
+++ python-sitemap-generator.py
@@ -220,7 +220,7 @@ class Sitemap:
 
     def xml(self):
         f = open(filename, "w")
-
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         print(
             etree.tostring(
                 self.urlset, pretty_print=True, encoding="unicode", method="xml"
